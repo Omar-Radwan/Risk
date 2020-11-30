abc8adb1afcfd883a0cc60ab15359d4c0e1bf9ed
Omar <omarabozeid99@gmail.com>
2020-12-01 01:11:34 +0200

    AggressiveAgent

---
+++ AggressiveAgent.py
@@ -0,0 +1,28 @@
+#send game state and return the new game state
+class AggressiveAgent:
+    def __init__(self):
+        self.cityList = []
+
+    def calculateBonusArmy(self):
+        return max(3, len(self.cityList)/3)
+
+    def calculateMaximumCity(self):
+        max = 0
+        for city in self.cityList:
+            if(city.armyCount > max):
+                max = city.armyCount
+                maxCity = city
+
+        return maxCity
+
+    def AggressiveAgentHeuristic(self):
+        bonusArmy = self.calculateBonusArmy()
+        maxCity = self.calculateMaximumCity()
+        print("bonus army is")
+        print(bonusArmy)
+        print("Maximum City before adding bonus army")
+        print(maxCity)
+        #add the bonusArmy to the maxCity in the game state and return it
+        maxCity.armyCount+=bonusArmy
+        print("Maximum City after adding bonus army")
+        print(maxCity)
