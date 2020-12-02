6e6532dbbeb26403f3469a1db3d32511b5347733
Omar Radwan <omarrm1999@gmail.com>
2020-12-02 18:54:41 +0200

    added NearlyPacifistAgent.py

---
+++ agents/nearly_pacifist_agent.py
@@ -0,0 +1,43 @@
+from typing import List, Any
+
+from Agent import Agent
+from City import City
+from Game import Game
+
+
+class NearlyPacifistAgent(Agent):
+
+    def compareCity(self, x: City, y: City) -> bool:
+        if (x.armyCount != y.armyCount):
+            return x.armyCount < y.armyCount
+        return x.id < y.id
+
+    def myMinArmyCity(self, game: Game) -> int:
+        minCity = self.cityList[0]
+        for city in self.cityList:
+            if (self.compareCity(city, minCity)):
+                minCity = city
+        return minCity.id
+
+    def hisMinArmyCityToAttack(self, game: Game) -> ():
+        myAdjToMin, hisMinCityId, graph = -1, -1, game.map.graph
+        for u in self.cityList:
+            uId = u.id
+            for vId in graph[uId]:
+                v = game.cityList[vId]
+                if u.armyCount - 2 >= v.armyCount:
+                    if hisMinCityId == -1:
+                        myAdjToMin, hisMinCityId = uId, vId
+                else:
+                    current, candidate = game.cityList[hisMinCityId], game.cityList[vId]
+                    if (self.compareCity(candidate, current)):
+                        myAdjToMin, hisMinCityId = uId, vId
+        return (myAdjToMin, hisMinCityId)
+
+    def applyHeuristic(self, game: Game, bonusPlayers: int):
+        myMinArmyCityId = self.myMinArmyCity(game)
+        u, v = self.hisMinArmyCityToAttack(game)
+        game.addSoldiersTo(myMinArmyCityId, bonusPlayers)
+        game.move(u, v, game.cityList[v].armyCount + 1)
+
+# should add bonus players to city id
--- minimax.py
+++ minimax.py
@@ -25,21 +25,34 @@ class MiniMaxAgent(Agent):
 
     def adjacentStates(self, game: Game, bonusPlayers: int, agent: Agent) -> []:
         result = []
-        for i in (0, bonusPlayers + 1):  # max number  = 60/3 -> 20
+        for i in (0, bonusPlayers + 1):  # max = 17
             maxi = (-1e9, None)
-            for c in agent.cityList:  # max number = 60
+            for c in agent.cityList:  # max number = 50
                 # create new copy of game state in which i soldiers are placed at city c
                 curGameState = copy.deepcopy(game)
                 curGameState.addSoldiersTo(c, i)
                 # try to do all moves and find the maximum evaluation
                 pass
 
-    def adjacentMoves(self, game: Game, bonusPlayers: int, agent: Agent) -> ():
-        for u in agent.cityList:  # max number = 60
-            for v in game.map.graph[u]:  # max number = 4
-                if (game.cityList[v].isRedArmy != game.cityList[v].isRedArmy and
-                        game.cityList[u] - 2 >= game.cityList[v]):  # two cities have different type of armies
-                    curGame = copy.deepcopy(game)
-                    curGame.move(u, v, curGame.cityList[
-                        v] + 1)  # conquer v from u with number of soldiers that can defeat soldiers in v
+    def adjacentMoves(self, game: Game, agent: Agent) -> ():
+        for u in agent.cityList:  # max number = 50
+            for v in game.map.graph[u]:  # max number = 5
+                q, r = game.cityList[u], game.cityList[v]
+                if (r.isRedArmy != r.isRedArmy and
+                        q.armyCount - 2 >= r.armyCount):  # two cities have different type of armies
+                    curGame = copy.deepcopy(game)  # a lot of work
+                    for i in (r.armyCount + 1, q.armyCount):  # conquer v with v.armyCount+1 .... u.armyCount-1
+                        pass
 
+# *
+# actions done in each turn:
+# place number of soldiers in a city <= 20
+# choose a city to conquer from all your cities <=30
+# choose number of soldiers to conquer that city with <= *#
+
+# *
+# soldiers
+# cities
+# safe cities except if he adds x soldier
+# cities can be attacked
+# *#
