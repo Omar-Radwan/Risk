16c0ec1505ee1b54b1edc6d6c55a793e91065ea6
Omar Radwan <omarrm1999@gmail.com>
2020-12-21 15:25:50 +0200

    fixed error in nearly_pacifist_agent.py

--- agents/nearly_pacifist_agent.py
+++ agents/nearly_pacifist_agent.py
@@ -1,5 +1,3 @@
-from typing import List, Any
-
 from agent import Agent
 from city import City
 from game import Game
@@ -10,34 +8,33 @@ class NearlyPacifistAgent(Agent):
     def compareCity(self, x: City, y: City) -> bool:
         return x.armyCount < y.armyCount if x.armyCount != y.armyCount else x.id < y.id
 
-    def myMinArmyCity(self, game: Game) -> int:
-        myCities = game.citiesOf(self.isRedPlayer)
-        minCity = myCities[0]
-        for city in myCities:
-            if (self.compareCity(city, minCity)):
-                minCity = city
-        return minCity.id
+    def myMinArmyCityId(self, game: Game, myCities: []) -> int:
+        myMinCityId = myCities[0]
+        for cityId in myCities:
+            if (self.compareCity(game.cityList[cityId], game.cityList[myMinCityId])):
+                myMinCityId = cityId
+        return myMinCityId
 
-    def hisMinArmyCityToAttack(self, game: Game) -> ():
-        myAdjToMin, hisMinCityId, graph = -1, -1, game.map.graph
-
-        for u in self.cityList:
-            uId = u.id
+    def hisMinArmyCityToAttack(self, game: Game, myCities: []) -> ():
+        bestUId, bestVId, graph = -1, -1, game.map.graph
+        for uId in myCities:
+            uCity = game.cityList[uId]
             for vId in graph[uId]:
-                v = game.cityList[vId]
-                if u.armyCount - 2 >= v.armyCount:
-                    if hisMinCityId == -1:
-                        myAdjToMin, hisMinCityId = uId, vId
+                vCity = game.cityList[vId]
+                if uCity.armyCount > vCity.armyCount + 1:
+                    if bestVId == -1:
+                        bestUId, bestVId = uId, vId
                 else:
-                    current, candidate = game.cityList[hisMinCityId], game.cityList[vId]
-                    if (self.compareCity(candidate, current)):
-                        myAdjToMin, hisMinCityId = uId, vId
-        return (myAdjToMin, hisMinCityId)
+                    if (self.compareCity(game.cityList[vId], game.cityList[bestVId])):
+                        bestUId, bestVId = uId, vId
+        return (bestUId, bestVId)
 
     def applyHeuristic(self, game: Game):
-        myMinArmyCityId = self.myMinArmyCity(game)
-        u, v = self.hisMinArmyCityToAttack(game)
-        game.addSoldiersToPlayer(myMinArmyCityId)
-        game.move(u, v, game.cityList[v].armyCount + 1)
+        myCities = game.citiesOf(self.isRedPlayer)
+        myMinArmyCityId = self.myMinArmyCityId(game, myCities)
+        game.addSoldiersToCity(myMinArmyCityId, game.bonusSoldiers(self.isRedPlayer))
 
-# should add bonus players to city id
+        uId, vId = self.hisMinArmyCityToAttack(game, myCities)
+        if uId != -1 and vId != -1:
+            game.move(uId, vId, game.cityList[vId].armyCount + 1)
+
